dfa541941ce9cd829b56fe909e2c42965f364d19
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-03-28 23:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstChar(text) {
   // your code here
+	 for (let char of text) {
+    // If the character is not a space, return it
+    if (char !== ' ') {
+      return char;
+    }
+  }
+  // If no non-space character is found, return an empty string
+  return '';
 }
 
 // Do not change the code below
